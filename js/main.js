04530181b5424accded20de12c3e124665bac6b3
Jeremy Ford <jeremyryanford@gmail.com>
2020-09-29 09:13:59 -0400

    hotjar updates
    Removed hotjar tracking code from main js file and added to index.html <head> instead

--- js/main.js
+++ js/main.js
@@ -99,13 +99,3 @@ fadeSplashOnScroll();
 setupLookBook();
 navLinkHighlightsOnScroll();
 triggerClipboard()
-
-// Hotjar Tracking Code for www.self-seekers.com
-(function(h,o,t,j,a,r){
-    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-    h._hjSettings={hjid:2015936,hjsv:6};
-    a=o.getElementsByTagName('head')[0];
-    r=o.createElement('script');r.async=1;
-    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-    a.appendChild(r);
-})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
